7ae093920c2035f53261bd2f49b0bb5999d108a3
Alex Kisakye <alex.kisakye@countercept.com>
2018-12-06 13:41:12 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # motd
 Shiny Messages of the Day for Linux Servers
+
+# Use
+When managing multiple Linux servers, you occassionally need a simple dashboard with info about the server you just logged into.
+This script manipulates the inherint motd feature of Unix like Operating Systems.
+
+# Usage
+Simply copy login_mod.sh to /etc/profile.d/login_mod.sh. If you have a large estate, you can also use config management like Puppet to copy and place the file for you
+
+#Feedback
+kisakye [at[ gmail.com
---
+++ login_mod.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+
+# Colors
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+# Basic info
+HOSTNAME=`uname -n`
+RO=`mount -v | fgrep 'on / ' | cut -d " " -f1`
+ROOT=`df -Ph | grep $RO | awk '{print $4}' | tr -d '\n'`
+IPADDR=`hostname -I`
+# System load
+MEMORY1=`free -t -m | grep Total | awk '{print $3" MB";}'`
+MEMORY2=`free -t -m | grep "Mem" | awk '{print $2" MB";}'`
+LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+HOGGERS=`ps -Ao user,uid,comm,pid,pcpu,pmem,tty --sort=-pcpu | head -n 6`
+
+echo "
+
+
+$(tput setaf 2)
+===============================================
+ - Hostname............: $HOSTNAME
+ - Disk Space..........: $ROOT remaining
+ - IP Address(es)......: $IPADDR
+===============================================
+ - CPU usage...........: $LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)
+ - Memory used.........: $MEMORY1 / $MEMORY2
+ - Swap in use.........: `free -m | tail -n 1 | awk '{print $3}'` MB
+===============================================
+ - Top 5 CPU and Mem hoggers
+
+$HOGGERS
+
+===============================================
+
+$(tput sgr 0)"
